c66a65180b86983a2c06c6db4ca9dabfa20a9de6
Denis <deniskamaldinov85@gmail.com>
2026-01-28 19:53:35 +0500

    Поменял даные отправки сообщения

--- server.ts
+++ server.ts
@@ -3,12 +3,6 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', (ws) => {
   console.log('Новый user подключился');
-
-  ws.send(JSON.stringify({
-    type: 'system',
-    message: "Добро пожаловать в чат",
-    time: new Date().toLocaleTimeString()
-  }))
   
   ws.on('message', (data: Buffer | JSON) => {
     const message = data.toString();
@@ -16,7 +10,7 @@ wss.on('connection', (ws) => {
 
     wss.clients.forEach((client) => {
       if (client.readyState === 1 && client !== ws) {
-        client.send(`${message}`)
+        client.send(JSON.stringify({message, type: 'chat'}))
       }
     })
   })
