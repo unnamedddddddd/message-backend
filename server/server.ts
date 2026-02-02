63c5082599717f4d46197d7c581a50e5fcbf7d39
Denis <deniskamaldinov85@gmail.com>
2026-02-02 21:15:21 +0500

    Добавил проверку на уже существующего пользователя с указанным логином

--- server/server.ts
+++ server/server.ts
@@ -44,15 +44,23 @@ pool.connect()
 app.post('/api/createUser', async (req, res) => {
   try {
     const {userLogin, userPassword} = req.body;
-    console.log(userLogin, userPassword);
     
+    const findUserByLogin = await pool.query('SELECT user_login FROM "Users" WHERE user_login = $1', [userLogin]);
+
+    if (findUserByLogin.rows.length > 0) {
+      return res.status(409).json({
+        success: false,
+        error: 'Логин уже занят',
+        message: 'Пользователь с таким логином уже существует',
+    });
+    }
 
     const result = await pool.query('INSERT INTO "Users"(user_login, user_password) VALUES($1,$2) RETURNING user_id', [userLogin, userPassword]);
 
     if (result.rows.length > 0) {
       console.log('Пользователь создан успешно', userLogin);
       
-      res.json({
+      return res.status(201).json({
         success: true,
         user_id: result.rows[0].user_id,
         message: 'Пользователь создан успешно',
@@ -60,12 +68,58 @@ app.post('/api/createUser', async (req, res) => {
     }
   } catch(error) {
       console.error(error);
-      res.json({
+      res.status(500).json({
         succees: false,
         message: error,
       })
   }
 })
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const {userLogin, userPassword} = req.body;
+
+    const result = await pool.query('SELECT user_login, user_password FROM "Users" WHERE user_login = $1 and user_password = $2', [userLogin, userPassword])
+
+    if (result.rows.length > 0) {
+      return res.status(200).json({
+        success: true,
+        user_id: result.rows[0].user_id,
+        message: 'Вход выполнен успешно',
+      })
+    }
+
+    return res.status(401).json({
+      success: false,
+      message: 'Такого пользователя не существует',
+    })
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: error,
+      })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 io.on('connection', (socket: SocketProps) => {
   console.log('User подключился', socket.id);
