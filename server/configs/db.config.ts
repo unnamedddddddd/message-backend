37b35740b16ee262239e71301d18dbeb9e825982
Denis <deniskamaldinov85@gmail.com>
2026-05-30 19:23:38 +0500

    fix

--- server/configs/db.config.ts
+++ server/configs/db.config.ts
@@ -7,6 +7,6 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.PGPORT),
   database: process.env.PGDATABASE,
-  user: process.env.DB_USER,
+  user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
 });
